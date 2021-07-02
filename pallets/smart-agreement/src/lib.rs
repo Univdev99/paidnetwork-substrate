cdeac19f048b75fd48b26b7bae5a32cdf8f430fb
UnivDev <univdev99@gmail.com>
2021-07-02 09:17:25 -0500

    Added structs and storage

--- pallets/smart-agreement/src/lib.rs
+++ pallets/smart-agreement/src/lib.rs
@@ -18,7 +18,12 @@ mod benchmarking;
 #[frame_support::pallet]
 pub mod pallet {
 	use frame_system::pallet_prelude::*;
-	use frame_support::{dispatch::DispatchResult, pallet_prelude::*, inherent::Vec, codec::{Encode, Decode}};
+	use frame_support::{
+		dispatch::DispatchResult,
+		pallet_prelude::*,
+		inherent::Vec,
+		codec::{Encode, Decode}
+	};
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
@@ -31,6 +36,34 @@ pub mod pallet {
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
+	#[derive(Encode, Decode, Clone, PartialEq)]
+	enum SmartAgreementState {
+		PendingSignature,
+		Accepted,
+		Declined,
+	}
+	impl Default for SmartAgreementState {
+		fn default() -> Self { SmartAgreementState::PendingSignature }
+	}
+
+	#[derive(Encode, Decode, Clone, PartialEq)]
+	enum AcceptanceState {
+		Empty,
+		Requested,
+		Accepted,
+		Declined,
+	}
+	impl Default for AcceptanceState {
+		fn default() -> Self { AcceptanceState::Empty }
+	}
+
+	#[derive(Encode, Decode, Default, Clone, PartialEq)]
+	pub struct Counterparty<T: Config> {
+		account: T::AccountId,
+		did: Vec<u8>,
+		acceptance_state: AcceptanceState,
+	}
+
 	#[derive(Encode, Decode, Default, Clone, PartialEq)]
 	pub struct AnchoredDocument<T: Config> {
 		file_hash: Vec<u8>,
@@ -40,8 +73,8 @@ pub mod pallet {
 		template_id: u128,
 		metadata: Vec<u8>,
 		valid_until: u128,
-		status: u8,
-		counterparty_addresses: Vec<T::AccountId>,
+		status: SmartAgreementState,
+		counterparties: Vec<T::AccountId>,
 		accepted_counterparties: u128,
 		declined_counterparties: u128,
 	}
@@ -51,6 +84,10 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn get_anchor)]
 	pub(super) type Anchor<T> = StorageMap<_, Twox64Concat, Vec<u8>, AnchoredDocument<T>>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn get_counterparty)]
+	pub(super) type Counterparties<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Counterparty<T>>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://substrate.dev/docs/en/knowledgebase/runtime/events
@@ -73,9 +110,14 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T:Config> Pallet<T> {
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn add_document(origin: OriginFor<T>, file_hash: Vec<u8>, proposer_did: Vec<u8>) -> DispatchResult {
+		pub fn add_document(
+			origin: OriginFor<T>,
+			file_hash: Vec<u8>,
+			proposer_did: Vec<u8>,
+			metadata: Vec<u8>
+		) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-			Self::deposit_event(Event::DocumentAnchored(who.clone(), file_hash.clone(), file_hash.clone()));
+			let counterparties: Vec<T::AccountId> = Vec::new();
 
 			let new_document = AnchoredDocument {
 				file_hash: file_hash.clone(),
@@ -83,15 +125,16 @@ pub mod pallet {
 				proposer_did: proposer_did.clone(),
 				required_quorum: 0,
 				template_id: 0,
-				metadata: Vec::new(),
+				metadata: metadata.clone(),
 				valid_until: 0,
-				status: 0,
-				counterparty_addresses: Vec::new(),
+				counterparties,
+				status: SmartAgreementState::PendingSignature,
 				accepted_counterparties: 0,
 				declined_counterparties: 0
 			};
 
 			<Anchor<T>>::insert(proposer_did.clone(), new_document);
+			Self::deposit_event(Event::DocumentAnchored(who.clone(), file_hash.clone(), file_hash.clone()));
 
 			Ok(())
 		}
